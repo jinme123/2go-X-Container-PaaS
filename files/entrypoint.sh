8cb8026b98a143bb37f0d26eb4bc18c4c80e7f2b
jinme123 <156739219+jinme123@users.noreply.github.com>
2024-02-16 23:17:07 +0800

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'607c33c4-8515-407b-b6b7-f8cb7e4b9718'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
 WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
